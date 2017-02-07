3e07fd086eea4a3badb0cac4545025706da12320
xuchunyangex <xuchunyang_3310@163.com>
2017-02-07 19:14:36 +0800

    Walkthrough 6 - modules

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,12 +1,21 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+	// wt06: Above here, add js files for new modules
+], function(Controller, MessageToast) {
+	// wt06: Above here, add control names, they can be used in code directly below
 	"use strict";
 
 	return Controller.extend("wt05Controllers.controller.View1", {
 		onShowHello : function () {
+			/* wt05 - Controllers
 			//show a native JS alert
 			alert("Hello world!");
+			*/
+			
+			/*wt06, after add MessageToast in 1.define 2.function;
+			now it could be used below*/
+			MessageToast.show("Hello world");
 		}
 	});
 
