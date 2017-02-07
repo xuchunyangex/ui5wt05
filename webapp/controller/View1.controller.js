2792517d3d458c7792af50e3e2a7f4d085835f2f
xuchunyangex <xuchunyang_3310@163.com>
2017-02-07 19:35:23 +0800

    walkthrough 07 Jsonmodel - 01

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,12 +1,18 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-	// wt06: Above here, add js files for new modules
-], function(Controller, MessageToast) {
-	// wt06: Above here, add control names, they can be used in code directly below
+	// wt06: Below here, add js files for new modules
+	"sap/m/MessageToast",
+	// wt07: JSONModel
+	"sap/ui/model/json/JSONModel"
+	
+// wt06: Above here, add control names, they can be used in code directly below
+], function(Controller, MessageToast, JSONModel) {
 	"use strict";
 
 	return Controller.extend("wt05Controllers.controller.View1", {
+		onInit : function () {
+		
+		},
 		onShowHello : function () {
 			/* wt05 - Controllers
 			//show a native JS alert
@@ -14,8 +20,12 @@ sap.ui.define([
 			*/
 			
 			/*wt06, after add MessageToast in 1.define 2.function;
-			now it could be used below*/
+			now it could be used below
 			MessageToast.show("Hello world");
+			*/
+			
+			// wt07, JSONmodel
+			
 		}
 	});
 
