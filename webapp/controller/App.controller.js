712b94f8ab8f47a3f0f254ba7947f1341fc15abe
xuchunyangex <xuchunyang_3310@163.com>
2017-02-10 14:05:58 +0800

    walkthrough 15 nested views finished

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,62 +1,18 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    //wt06: Below here, add js files for new modules
-    "sap/m/MessageToast",
-    //wt07: JSONModel
-    "sap/ui/model/json/JSONModel",
-    //wt08: Translate texts
-    "sap/ui/model/resource/ResourceModel"
+    // //wt06: Below here, add js files for new modules
+    // "sap/m/MessageToast",
+    // //wt07: JSONModel
+    // "sap/ui/model/json/JSONModel",
+    // //wt08: Translate texts
+    // "sap/ui/model/resource/ResourceModel"
 
     // wt06: Above here, add control names, they can be used in code directly below
 ], function(Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
 
     return Controller.extend("wt05Controllers.controller.App", {
-    	/*in wt09, the model 
-        //onInit is cimilar to a constructor function of a control
-        onInit: function() {
-            // wt07, jsonmodel, set data model on view
-            // set during onInit
-            var oData = {
-                recipient: {
-                    name: "World_wt08 One"
-                }
-            };
-            var oModel = new JSONModel(oData);
-            this.getView().setModel(oModel, "recip");
-            //wt08, set i18n model on view, 
-            //ResourceModel => i18n fiel
-            var i18nModel = new ResourceModel({
-                //bundle name is the i18n file, with name space and path
-                bundleName: "wt05Controllers.i18n.i18n"
-            });
-            //setModel(oModel, sName?)Sets or unsets a model for the given model name for this ManagedObject.
-            this.getView().setModel(i18nModel, "i18n");
-        },
-        */
 
-        onShowHello: function() {
-            /* wt05 - Controllers
-            //show a native JS alert
-            alert("Hello world!");
-            */
-
-            /*wt06, after add MessageToast in 1.define 2.function;
-            now it could be used below
-            MessageToast.show("Hello World");
-            */
-
-            //wt08, read msg from i18n model
-            var oBundle = this.getView().getModel("i18n").getResourceBundle();
-            //The getProperty method can be called in any model and takes the data path as an argument.
-            var sRecipient0 = this.getView().getModel("recip").getProperty("/recipient/name");
-            var sRecipient1 = "Two";
-            var sRecipient2 = "Three";
-            //the resource bundle has a specific getText method that takes an array of strings as second argument.
-            var sMsg = oBundle.getText("helloMsg", [sRecipient0, sRecipient1, sRecipient2]);
-            MessageToast.show(sMsg);
-
-        }
     });
 
 });
---
+++ webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,57 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function(Controller, MessageToast) {
+	"use strict";
+
+	return Controller.extend("wt05Controllers.controller.HelloPanel", {
+		//wt15 nested view, the controller for hello panel is new created and onShowHello event is moved here
+        onShowHello: function() {
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            //The getProperty method can be called in any model and takes the data path as an argument.
+            var sRecipient0 = this.getView().getModel("recip").getProperty("/recipient/name");
+            var sRecipient1 = "Two";
+            var sRecipient2 = "Three";
+            //the resource bundle has a specific getText method that takes an array of strings as second argument.
+            var sMsg = oBundle.getText("helloMsg", [sRecipient0, sRecipient1, sRecipient2]);
+            MessageToast.show(sMsg);
+
+        }
+		/**
+		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+		 * @memberOf namespaceXXX.view.viewXXX
+		 */
+			// onInit: function() {
+		
+			// },
+
+		/**
+		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+		 * (NOT before the first rendering! onInit() is used for that one!).
+		 * @memberOf namespaceXXX.view.viewXXX
+		 */
+		//	onBeforeRendering: function() {
+		//
+		//	},
+
+		/**
+		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+		 * This hook is the same one that SAPUI5 controls get after being rendered.
+		 * @memberOf namespaceXXX.view.viewXXX
+		 */
+		//	onAfterRendering: function() {
+		//
+		//	},
+
+		/**
+		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+		 * @memberOf namespaceXXX.view.viewXXX
+		 */
+		//	onExit: function() {
+		//
+		//	}
+
+	});
+
+});
