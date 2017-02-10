1a2c5101ad6b04ebe4af5832c2462ddbc02b93ad
xuchunyangex <xuchunyang_3310@163.com>
2017-02-10 15:56:26 +0800

    walkthrough 17 fragement callback finished

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,11 +1,11 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
 ], function(Controller, MessageToast) {
-	"use strict";
+    "use strict";
 
-	return Controller.extend("wt05Controllers.controller.HelloPanel", {
-		//wt15 nested view, the controller for hello panel is new created and onShowHello event is moved here
+    return Controller.extend("wt05Controllers.controller.HelloPanel", {
+        //wt15 nested view, the controller for hello panel is new created and onShowHello event is moved here
         onShowHello: function() {
             var oBundle = this.getView().getModel("i18n").getResourceBundle();
             //The getProperty method can be called in any model and takes the data path as an argument.
@@ -18,58 +18,64 @@ sap.ui.define([
 
         },
 
-// wt16 dialog with fragment
-        onOpenDialog : function () {
-        	var oView = this.getView();
-        	var oDialog = oView.byId("helloDialog");
-        	//create dialog lazily
-        	if (!oDialog) {
-        		//create dialog via fragment factory
-        		//here ID of the fragment "helloDialog" = oView.getId() .
-        		// oDialog = sap.ui.xmlfragment("helloDialog", "wt05Controllers.view.HelloDialog", this);
-        		// oDialog = sap.ui.xmlfragment("wt05Controllers.view.HelloDialog", this);
-        		oDialog = sap.ui.xmlfragment(oView.getId(), "wt05Controllers.view.HelloDialog", this);
-        		oView.addDependent(oDialog);
-        	}
+        // wt16 dialog with fragment
+        onOpenDialog: function() {
+            var oView = this.getView();
+            var oDialog = oView.byId("helloDialog");
+            //create dialog lazily
+            if (!oDialog) {
+                //create dialog via fragment factory
+                //here ID of the fragment "helloDialog" = oView.getId() .
+                // oDialog = sap.ui.xmlfragment("helloDialog", "wt05Controllers.view.HelloDialog", this);
+                // oDialog = sap.ui.xmlfragment("wt05Controllers.view.HelloDialog", this);
+                oDialog = sap.ui.xmlfragment(oView.getId(), "wt05Controllers.view.HelloDialog", this);
+                oView.addDependent(oDialog);
+            }
 
-        	oDialog.open();
+            oDialog.open();
 
+        },
+
+        //wt17 fragment callbacks
+        onCloseDialog: function() {
+            this.getView().byId("helloDialog").close();
         }
-		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
-		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf namespaceXXX.view.viewXXX
-		 */
-			// onInit: function() {
-		
-			// },
 
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf namespaceXXX.view.viewXXX
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
+        /**
+         * Called when a controller is instantiated and its View controls (if available) are already created.
+         * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+         * @memberOf namespaceXXX.view.viewXXX
+         */
+        // onInit: function() {
 
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf namespaceXXX.view.viewXXX
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
+        // },
 
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf namespaceXXX.view.viewXXX
-		 */
-		//	onExit: function() {
-		//
-		//	}
+        /**
+         * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+         * (NOT before the first rendering! onInit() is used for that one!).
+         * @memberOf namespaceXXX.view.viewXXX
+         */
+        //	onBeforeRendering: function() {
+        //
+        //	},
 
-	});
+        /**
+         * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+         * This hook is the same one that SAPUI5 controls get after being rendered.
+         * @memberOf namespaceXXX.view.viewXXX
+         */
+        //	onAfterRendering: function() {
+        //
+        //	},
+
+        /**
+         * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+         * @memberOf namespaceXXX.view.viewXXX
+         */
+        //	onExit: function() {
+        //
+        //	}
+
+    });
 
 });
