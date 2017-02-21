e4912a453c0569aa1149fe03e1c4fbfe49070d43
xuchunyangex <xuchunyang_3310@163.com>
2017-02-21 23:43:52 +0800

    walkthrough 29 Integration Test with OPA part 1

---
+++ webapp/test/integration/navigationJourney.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+	"sap/ui/test/opaQunit"
+], function(opaTest) {
+	"use strict";
+
+	QUnit.module("Navigation");
+
+	opaTest("Should open the hello dialog", function(Given, When, Then) {
+		// Arrangements
+		Given.iStartMyAppInAFrame();
+		
+		// Actions
+		When.onTheAppPage.iPressTheSayHelloWithDialogButton();
+		
+		// Assertations
+		Then.onTheAppPage.iShouldSeeTheHelloDialog().and.iTeardownMyAppFrame();
+	});
+
+});
